eecaa12b7d0d2316f80b6368dd71fc6551301d54
Adam H <ad.hauck@gmail.com>
2024-05-15 01:54:07 -0700

    Added Nav & Main Page with Routes

---
+++ src/components/navigation.jsx
@@ -0,0 +1,24 @@
+import { Link, Outlet } from 'react-router-dom'
+
+function Navigation() {
+    return (
+    <>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>
+                            Home
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div>
+            <Outlet />
+        </div>
+    </>
+    )
+}
+
+export default Navigation;
