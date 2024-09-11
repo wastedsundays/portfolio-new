468130afb8d4abad6f3c11cd53733c19f38b1654
Adam H <ad.hauck@gmail.com>
2024-09-11 15:57:40 -0700

    added custom svg icons for footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,8 @@
 import { Link } from "react-router-dom";
-// import { FaGithub } from "react-icons/fa6";
-import { FaSquareGithub } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa6";
-// import { FaEnvelope } from "react-icons/fa6";
-import { FaSquareEnvelope } from "react-icons/fa6";
+
+import TwitterIcon from "../icons/TwitterIcon";
+import LinkedinIcon from "../icons/LinkedinIcon";
+import GithubIcon from "../icons/GithubIcon";
 
 const Footer = () => {
 
@@ -19,17 +18,18 @@ const Footer = () => {
                 <ul>
                     <li>
                         <a href='https://www.linkedin.com/in/adamhauck1/'>
-                        <FaLinkedin />
+                        <LinkedinIcon />
+                        
                         </a>
                     </li>
                     <li>
                         <a href='https://github.com/wastedsundays'>
-                        <FaSquareGithub />
+                        <GithubIcon />
                         </a>
                     </li>
                     <li>
                         <Link to={`/contact`}>
-                        <FaSquareEnvelope />
+                        <TwitterIcon />
                         </Link>
                     </li>
                 </ul>
